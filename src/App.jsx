6473159aa5a7b46a5e30ae416a84627003bc1ca4
Zeshan Saleem <zeshan155045@gmail.com>
2026-01-14 21:00:30 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <h1 className="text-5xl bg-black text-green-500 p-5">
+  Tailwind Working
+</h1>
+
+
+    </>
+  )
+}
+
+export default App
